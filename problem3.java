23e8af9e9fe5c72070ad6a49f6d10e5d49b94eb5
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-02-09 14:34:38 -0500

    fin
    fin

---
+++ problem3.java
@@ -0,0 +1,5 @@
+public class problem3 {
+    public static void main (String[]args){
+        System.out.println("\tAnderson\t 3452\n \tBoyton\t \t 5344\n \tCarrington \t 3677\n \tHouston\t \t 5988\n \tMcDougal\t 5233\n \tRobertson\t 3623\n \tStryker\t \t 511");
+    }
+}
